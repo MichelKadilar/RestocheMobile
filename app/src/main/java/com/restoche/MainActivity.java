d218ee8830a8c495d1f7a36158faa7ba29841d9f
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-05-09 17:14:15 +0200

    Refactor avec integration des fragments et gestion de la connexion et de l'incription grace au viewModel

--- app/src/main/java/com/restoche/MainActivity.java
+++ app/src/main/java/com/restoche/MainActivity.java
@@ -1,41 +1,27 @@
 package com.restoche;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+
+import fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // initialise login button
-        loginButton = findViewById(R.id.button);
-        // Set an OnClickListener for the login button
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO: handle login functionality
-            }
-        });
-        // Initialize the "Register" TextView
-        LinearLayout registerTextView = findViewById(R.id.s_inscrire);
 
-        // Set an OnClickListener for the "Register" TextView
-        registerTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Open the "Register" activity
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
+        // Load the LoginFragment into the container
+        loadLoginFragment();
     }
 
+    private void loadLoginFragment() {
+        LoginFragment loginFragment = new LoginFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, loginFragment);
+        transaction.commit();
+    }
 }
---
+++ app/src/main/java/viewModels/LoginViewModel.java
@@ -0,0 +1,36 @@
+package viewModels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+    private MutableLiveData<String> _email = new MutableLiveData<>();
+    private MutableLiveData<String> _password = new MutableLiveData<>();
+
+    public LiveData<String> getEmail() {
+        return _email;
+    }
+
+    public LiveData<String> getPassword() {
+        return _password;
+    }
+
+    public void onEmailChanged(String email) {
+        _email.setValue(email);
+    }
+
+    public void onPasswordChanged(String password) {
+        _password.setValue(password);
+    }
+
+    public boolean validateInputs() {
+        // Validez les entrées et renvoyez true si elles sont valides, sinon false
+        return false; // Retournez la valeur correcte après avoir implémenté la logique de validation
+    }
+
+    public boolean authenticate() {
+        // Authentifiez l'utilisateur et renvoyez true si la connexion est réussie, sinon false
+        return false; // Retournez la valeur correcte après avoir implémenté la logique d'authentification
+    }
+}
