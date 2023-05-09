df224739f9cbbb2715179cdf80051c81d30f8e60
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-05-09 12:54:09 +0200

    Creation d'intenet et mobilité entre la page de connexion et d'inscription

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,5 @@
 
-r
-Bapp/src/androidTest/java/com/restoche/ExampleInstrumentedTest.java,6\d\6d38e95ed849f208f6066adca2d5fef98aef770d
 c
 3app/src/test/java/com/restoche/ExampleUnitTest.java,9\1\918d98ae01ae024630c85b842abf169eb6073b5d
+w
+G.idea/sonarlint/issuestore/9/1/918d98ae01ae024630c85b842abf169eb6073b5d,3\0\30e1e8fe2ac8cf964d7b14077e10bca18c5aebe5
--- app/src/main/java/com/restoche/RegisterActivity.java
+++ app/src/main/java/com/restoche/RegisterActivity.java
@@ -1,7 +1,54 @@
 package com.restoche;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
-    p
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
+    private Button registerButton;
+    private TextView loginTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_view);
+
+        // Initialize UI elements
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        confirmPasswordEditText = findViewById(R.id.editTextTextPasswordConfirm);
+        registerButton = findViewById(R.id.button);
+        loginTextView = findViewById(R.id.textView3);
+
+        // Set an OnClickListener for the Register button
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Add code to register the user
+                // For example, you could validate the input fields
+                // and add the new user to a database
+                Toast.makeText(RegisterActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                finish(); // Close the activity
+            }
+        });
+
+        // Set an OnClickListener for the "Login" TextView
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open the "Login" activity
+                // TODO: Implement the login activity if it hasn't been implemented yet
+                finish(); // Close the current activity
+            }
+        });
+    }
 }
