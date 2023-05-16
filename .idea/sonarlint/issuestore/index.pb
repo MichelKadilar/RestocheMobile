4af9f1247241ba7ab5c77f783250367f6bb6aa3a
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-05-16 15:49:18 +0200

    Refactor avec integration des fragments et gestion de la register et de l'incription grace au viewModel

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,9 @@ c
 3app/src/test/java/com/restoche/ExampleUnitTest.java,9\1\918d98ae01ae024630c85b842abf169eb6073b5d
 w
 G.idea/sonarlint/issuestore/9/1/918d98ae01ae024630c85b842abf169eb6073b5d,3\0\30e1e8fe2ac8cf964d7b14077e10bca18c5aebe5
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+r
+Bapp/src/androidTest/java/com/restoche/ExampleInstrumentedTest.java,6\d\6d38e95ed849f208f6066adca2d5fef98aef770d
+w
+G.idea/sonarlint/issuestore/3/0/30e1e8fe2ac8cf964d7b14077e10bca18c5aebe5,7\6\76dbc55868bac27637dd298c0e1c5af335a5d29a
---
+++ app/src/main/java/fragments/RegisterFragment.java
@@ -0,0 +1,58 @@
+package fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.restoche.R;
+
+public class RegisterFragment extends Fragment {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextPasswordConfirm;
+    private Button registerButton;
+    private TextView loginTextView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editTextEmail = view.findViewById(R.id.editTextTextEmailAddress);
+        editTextPassword = view.findViewById(R.id.editTextTextPassword);
+        editTextPasswordConfirm = view.findViewById(R.id.editTextTextPasswordConfirm);
+        registerButton = view.findViewById(R.id.button);
+        loginTextView = view.findViewById(R.id.textView3);
+
+        // Set an OnClickListener for the register button
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: handle registration functionality
+            }
+        });
+
+        // Set an OnClickListener for the login TextView
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: navigate to the login fragment
+            }
+        });
+    }
+}
