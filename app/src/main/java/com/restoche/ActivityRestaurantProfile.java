3e05f397fc605a662b9646b235c7614d3049650c
MichelK <michel.kadilar@etu.unice.fr>
2023-05-18 17:28:57 +0200

    R-2 : Layout restaurant profile : back arrow missing

---
+++ app/src/main/java/com/restoche/ActivityRestaurantProfile.java
@@ -0,0 +1,15 @@
+package com.restoche;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityRestaurantProfile extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_profile);
+    }
+}
---
+++ app/src/main/java/com/restoche/FragmentRestaurantProfileRatingsAndComments.java
@@ -0,0 +1,84 @@
+package com.restoche;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentRestaurantProfileRatingsAndComments extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_restaurant_profile_ratings_and_comments, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerview);
+
+        // Initialiser votre adapter et layoutManager
+        List<String> l = new ArrayList<>();
+        l.add("string1");
+        l.add("string1");
+        l.add("string1");
+        l.add("string1");
+        l.add("string1");
+        l.add("string1");
+        l.add("string1");
+        adapter = new MyAdapter(l);
+        layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+
+    public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        private List<String> dataList; // Vos données à afficher
+
+        public MyAdapter(List<String> dataList) {
+            this.dataList = dataList;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_rankings_and_comments_item, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            String data = dataList.get(position);
+            //holder.textView.setText(data);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataList.size();
+        }
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(R.id.textView);
+            }
+        }
+    }
+}
