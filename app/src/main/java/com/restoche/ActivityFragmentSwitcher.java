b8e90aeb125579ef67b3bd15440c9e224edb3877
MichelK <michel.kadilar@etu.unice.fr>
2023-05-18 20:47:31 +0200

    R-2 : add of bottom nav menu to switch between tabs

---
+++ app/src/main/java/com/restoche/ActivityFragmentSwitcher.java
@@ -0,0 +1,14 @@
+package com.restoche;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityFragmentSwitcher extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_fragment_switcher);
+    }
+}
