06636c61f8f02e2f06c9b91b1fe302d5c289a200
MichelK <michel.kadilar@etu.unice.fr>
2023-05-20 20:16:25 +0200

    R-2 : fragments switching working

--- app/src/main/java/com/restoche/ActivityFragmentSwitcher.java
+++ app/src/main/java/com/restoche/ActivityFragmentSwitcher.java
@@ -1,14 +1,52 @@
 package com.restoche;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class ActivityFragmentSwitcher extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+public class ActivityFragmentSwitcher extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
+
+    private int currentSelectedItemId;
+    private BottomNavigationView bottomNavigationView;
+
+    private FragmentManager fm;
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_fragment_switcher);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        fm = getSupportFragmentManager();
+        /*ft = fm.beginTransaction();
+        ft.add(R.id.frame_fragment_container, new FragmentParameters());
+        ft.commit();*/
+
+        this.currentSelectedItemId = bottomNavigationView.getMenu().getItem(0).getItemId();
+        bottomNavigationView.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int clickedItemId = item.getItemId();
+        switch (clickedItemId) {
+            case R.id.access_parameters -> { // not safe to use ids because not final/unique when builded
+                fm.beginTransaction().replace(R.id.frame_fragment_container, new FragmentParameters()).commit();
+                return true;
+            }
+            default -> {
+                return false;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/restoche/FragmentParameters.java
@@ -0,0 +1,22 @@
+package com.restoche;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentParameters extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_parameters, container, false);
+
+        return v;
+    }
+}
