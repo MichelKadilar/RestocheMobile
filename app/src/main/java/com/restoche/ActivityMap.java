2dbdc28edc0931b4feabbb95243ccfd26b4af54e
MichelK <michel.kadilar@etu.unice.fr>
2023-05-20 13:31:57 +0200

    R-2 : back button working & some minor corrections

--- app/src/main/java/com/restoche/ActivityMap.java
+++ app/src/main/java/com/restoche/ActivityMap.java
@@ -1,15 +1,25 @@
 package com.restoche;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityMap extends AppCompatActivity {
 
+    private LinearLayout goBack;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        goBack = findViewById(R.id.LL_go_back);
+        goBack.setOnClickListener(view -> {
+            finish();
+        });
     }
 }
