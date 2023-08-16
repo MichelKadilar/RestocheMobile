9d7126928c6f9f5e8515accd90dcce3f8ad6dffa
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-08-16 02:59:40 +0200

    beurk

--- app/src/main/java/fragments/FragmentUserActivity.java
+++ app/src/main/java/fragments/FragmentUserActivity.java
@@ -1,9 +1,12 @@
 package fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,16 +15,32 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.restoche.R;
+import com.bumptech.glide.Glide;  // Assurez-vous d'avoir cette dépendance si vous utilisez Glide
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
+import models.Resto;
+
 public class FragmentUserActivity extends Fragment {
 
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
+    private MyAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    private DatabaseReference mDatabase;
+
 
     @Nullable
     @Override
@@ -29,30 +48,44 @@ public class FragmentUserActivity extends Fragment {
         View view = inflater.inflate(R.layout.fragment_user_activity, container, false);
 
         recyclerView = view.findViewById(R.id.RV_user_activity_list);
-
-        // Initialiser votre adapter et layoutManager
-        List<String> l = new ArrayList<>();
-        l.add("string1");
-        l.add("string1");
-        l.add("string1");
-        l.add("string1");
-        l.add("string1");
-        l.add("string1");
-        l.add("string1");
-        adapter = new MyAdapter(l);
         layoutManager = new LinearLayoutManager(getActivity());
-
-        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(layoutManager);
 
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+        mDatabase.child("restos").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Resto> restaurantsList = new ArrayList<>();
+                for (DataSnapshot restaurantSnapshot : dataSnapshot.getChildren()) {
+                    Resto restaurant = restaurantSnapshot.getValue(Resto.class);
+                    restaurantsList.add(restaurant);
+                }
+
+                // Mise à jour de l'adapter avec les nouvelles données
+                adapter = new MyAdapter(restaurantsList);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("FragmentUserActivity", "Erreur de chargement des données", databaseError.toException());
+            }
+        });
+    }
+
     public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-        private List<String> dataList; // Vos données à afficher
+        private List<Resto> dataList;
 
-        public MyAdapter(List<String> dataList) {
+        public MyAdapter(List<Resto> dataList) {
             this.dataList = dataList;
         }
 
@@ -65,8 +98,22 @@ public class FragmentUserActivity extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
-            String data = dataList.get(position);
-            //holder.textView.setText(data);
+            Resto resto = dataList.get(position);
+
+
+            int radius = 830; // rayon pour les coins arrondis
+            int margin = 0;  // marge à utiliser si nécessaire
+            RoundedCornersTransformation transformation = new RoundedCornersTransformation(radius, margin);
+
+            Picasso.get()
+                    .load(resto.getImage())
+                    .transform(transformation)
+                    .into(holder.imageView);
+
+
+            holder.titleView.setText(resto.getTitle());
+            holder.ratingBar.setRating(resto.getRatings());
+            holder.descriptionView.setText(resto.getDescription());
         }
 
         @Override
@@ -75,11 +122,17 @@ public class FragmentUserActivity extends Fragment {
         }
 
         public static class MyViewHolder extends RecyclerView.ViewHolder {
-            TextView textView;
+            ImageView imageView;
+            TextView titleView;
+            RatingBar ratingBar;
+            TextView descriptionView;
 
             public MyViewHolder(@NonNull View itemView) {
                 super(itemView);
-                textView = itemView.findViewById(R.id.textView);
+                imageView = itemView.findViewById(R.id.imageView4);
+                titleView = itemView.findViewById(R.id.textView20);
+                ratingBar = itemView.findViewById(R.id.ratingBar);
+                descriptionView = itemView.findViewById(R.id.textView22);
             }
         }
     }
